56ad1952a0dfdcd85a07a622b2c911d6dc7804e4
yuroyoro <ozaki@yuroyoro.com>
2010-04-28 16:51:32 +0900

    accessToken取得まで

---
+++ src/main/scala/com/yuroyoro/util/net/package.scala
@@ -0,0 +1,36 @@
+package com.yuroyoro.util
+
+import scala.xml.NodeSeq
+import java.net.{URL,URI,HttpURLConnection,URLEncoder}
+
+package object net {
+
+  val byteEncoding = "UTF-8"
+  def bytes( s:String ) = s.getBytes( byteEncoding )
+
+  def encodeURL( p:String ) = URLEncoder.encode( p ,byteEncoding)
+
+  def encodeBase64( p:String ):String = Base64.encode( p )
+  def encodeBase64( b:Array[Byte]):String = Base64.encodeBytes( b )
+  // def encodeBase64( p:String ) = {
+  //   val encoder = new sun.misc.BASE64Encoder
+  //   encoder.encode( bytes(p) )
+  // }
+  def normalize( url:String ) = (new URI( url)).normalize.toString
+
+
+  def queryStrings[A,B]( params:(A,B) *) =
+    params.map{ case ( k, v ) => "%s=%s" format(k,v) }.toList.mkString("&")
+
+  def encodedQueryStrings[A,B]( params:(A,B) *) =
+    params.map{ case ( k, v ) => "%s=%s" format(encodeURL(k.toString),encodeURL(v.toString)) }.toList.mkString("&")
+
+  implicit def nodeSeq2HtmlNodeSeq( xml:NodeSeq ) = new HtmlNodeSeq( xml )
+  implicit def map2QueryStrings[A,B]( params:Map[A,B] ) = new QueryStringMap( params )
+
+  class QueryStringMap[A,B]( params:Map[A,B]){
+    def toQueryStrings = queryStrings( params.toList : _* )
+    def toEncodedQueryStrings = queryStrings( params.toList : _* )
+  }
+}
+
