95826fd272102bea6221b888cdf678c11a7fd3f7
yuroyoro <ozaki@yuroyoro.com>
2010-06-13 16:24:29 +0900

    TwitterのOAuthにあわせて微調整

---
+++ src/main/scala/com/yuroyoro/util/io/Image.scala
@@ -0,0 +1,33 @@
+package com.yuroyoro.util.io
+
+// import java.io.{ File => JFile }
+import java.net.URL
+import java.io.{ByteArrayOutputStream,BufferedOutputStream }
+import java.awt.image.BufferedImage
+import javax.imageio.ImageIO
+
+class Image( val image:BufferedImage ) {
+
+  def bytes( format:String = "png") = {
+    val bos =  new ByteArrayOutputStream
+    val os = new BufferedOutputStream(bos)
+    image.flush
+    ImageIO.write(image, format, os);
+    os.flush
+    os.close
+    bos.toByteArray
+  }
+
+  def write( file:String ,  format:String = "png"):Image = {
+    ImageIO.write( image, format , new JFile(file) )
+    this
+  }
+}
+
+object Image {
+  def apply( url:String ):Image = new Image(
+    if( url.contains(':') ) ImageIO.read( new URL(url) )
+    else ImageIO.read( new JFile( url ) ) )
+
+}
+
---
+++ src/main/scala/com/yuroyoro/util/io/package.scala
@@ -0,0 +1,31 @@
+package com.yuroyoro.util
+
+package object io {
+  import scala.io.{Source, Codec}
+  import scala.xml.XML
+  import scala.xml.parsing.XhtmlParser
+  import java.io.{InputStream, ByteArrayOutputStream,BufferedOutputStream }
+
+  type JFile = java.io.File
+
+  def source( url:String )(implicit codec: Codec = Codec.default) =
+    if( url.contains(':') ) Source.fromURL( new java.net.URL(url) )
+    else Source.fromPath( url )
+
+  def loadXml( url:String ) =
+    if( url.contains(':') ) XML.load( new java.net.URL(url))
+    else XML.loadFile( url )
+
+  def loadXHtml( url:String ) = XhtmlParser( SourceEx( url ))
+
+  def toByteArray( in:InputStream ) = {
+    val buf = new Array[Byte](1024)
+    val bos =  new ByteArrayOutputStream
+    val os = new BufferedOutputStream(bos)
+    for(i <- Stream.continually(in.read(buf)).takeWhile(_ != -1)){
+      os.write( buf )
+    }
+    bos.toByteArray
+  }
+}
+
